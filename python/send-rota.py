597844c9ff4e064f6c5b3095b233d639c7087fcc
Tim Stephenson <tim@knowprocess.com>
2019-10-13 15:54:24 +0100

    update script to reflect removed data

--- python/send-rota.py
+++ python/send-rota.py
@@ -9,23 +9,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from_me = 'info@corshambaptists.org'
-to_you = 'tim@thestephensons.me.uk'
+to_you = 'tim@thestephensons.me.uk,vicky.stephenson@gmail.com'
+bcc_you = 'data@corshambaptists.org'
 # the first week in the spreadsheet (will always be a Sunday)
-sun1 = datetime(2018,7,15)
+sun1 = datetime(2019,10,6)
 # calculate next sunday date
 now = datetime.now()
 sun2 = (now + timedelta(weeks=1) - timedelta(days=now.isoweekday()))
 # No. of weeks passed (2 non-data columns hence add 2 to start on C)
 weeks = ((sun2 - sun1).days / 7) + 2
+print(weeks)
 # work out which column we want
 CHAR1 = " ABCDEFGHIJKLMNOPQRSTUVWXY"
 CHAR2 = "ABCDEFGHIJKLMNOPQRSTUVWXY"
 col = None
-if (weeks <= 26):
+if (weeks <= 24):
+  print('single char col')
   col = CHAR2[int(weeks % 26):int(weeks % 26)+1]
 else:
+  print('double char col')
   col = CHAR1[int(weeks / 26):int(weeks / 26)+1] + CHAR2[int(weeks % 26):int(weeks % 26)+1]
-#print(col)
+print(col)
 
 html = os.popen("rota " + col).read()
 print(html)
@@ -35,6 +39,7 @@ msg['Subject'] = "Plan for 9:15 on " + sun2.strftime("%d %b")
 print(msg['Subject'])
 msg['From'] = from_me 
 msg['To'] = to_you
+msg['Bcc'] = bcc_you
 text_part = MIMEText("Please view in an HTML capable mail reader", 'plain')
 html_part = MIMEText(html, 'html')
 
