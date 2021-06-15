e6fbe486312819ef020b92deadd0c50b0a19afa1
Tim Stephenson <tim@knowprocess.com>
2021-06-15 12:27:58 +0100

    tidy logging and return code

--- send-rota/rota.py
+++ send-rota/rota.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# requirements: pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+# 
+# Read rota sheet and email to admin for further editing
+# Install requirements: pip3 install -r requirements.txt
 
 from __future__ import print_function
 
@@ -7,11 +9,12 @@ import argparse
 from datetime import datetime
 from datetime import timedelta
 import os
+import os.path
 import smtplib
+import sys
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -24,13 +27,12 @@ SHEET_NAME = 'Rota'
 NO_HDR_COLS = 1
 
 FROM = 'info@corshambaptists.org'
-TO = 'tims@corshambaptists.org'
-bcc_you = 'data@corshambaptists.org'
+TO = 'office@corshambaptists.org'
+CC = 'info@corshambaptists.org'
+# the first week in the spreadsheet (must always be a Sunday)
+SUN1 = datetime(2021,1,3)
 
 args = None
-
-# the first week in the spreadsheet (must always be a Sunday)
-sun1 = datetime(2021,1,3)
 
 def calcSunday():
   now = datetime.now()
@@ -42,7 +44,7 @@ def calcSunday():
 
 def calcWeek(sun2):
   # No. of weeks passed (1 column header means add 1 to start on B etc)
-  weeks = ((sun2 - sun1).days / 7) + NO_HDR_COLS
+  weeks = ((sun2 - SUN1).days / 7) + NO_HDR_COLS
   if args.verbose:
     print('  column offset is: {} ...'.format(int(weeks)))
   return int(weeks)
@@ -97,7 +99,7 @@ def composeMessage(spreadsheet, sheetName, weeks, col):
                                 range=range).execute()
     values = result.get('values', [])
 
-    html = "<html><body><strong>LEADERS COPY, PLEASE EDIT AS NEEDED AND BCC TO CONGREGATION</strong><p>Hi everyone,</p><p>Here's the plan for this Sunday. If you have any issues please try to arrange a swap."
+    html = "<html><body><strong>DRAFT, PLEASE EDIT AS NEEDED AND FORWARD AS APPROPRIATE</strong><p>Hi everyone,</p><p>Here's the plan for this Sunday. If you have any issues please try to arrange a swap."
 
     if not values:
         print('No data found.')
@@ -112,7 +114,8 @@ def composeMessage(spreadsheet, sheetName, weeks, col):
               if row[weeks] != None and row[weeks] != '-':
                 html += ('<li><strong>%s:</strong> %s</li>\n' % (row[0], row[weeks]))
             except:
-              print('  no data in row {} and col {}'.format(idx, weeks))
+              if args.verbose:
+                print('  no data in row {} and col {}'.format(idx, weeks))
         html += '</ul>'
 
         #html += "<p><em>Looking ahead to next week, "
@@ -131,8 +134,7 @@ def composeMessage(spreadsheet, sheetName, weeks, col):
         html += "<p>The master list is: <a href='https://docs.google.com/spreadsheets/d/%s/'>here.</a></p>\n" % spreadsheet
         html += "<p>All the best,</body></html>"
 
-    if args.verbose:
-        print('  composed msg: {}'.format(html))
+    print('Message:\n{}'.format(html))
     return html
 
 def parseArgs():
@@ -141,13 +143,12 @@ def parseArgs():
       action="store_true")
   return parser.parse_args()
 
-def sendMail(html):
+def sendMail(subject, html):
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = "Plan for Sunday Service on " + sun2.strftime("%d %b")
-    print(msg['Subject'])
     msg['From'] = FROM
     msg['To'] = TO
-    msg['Bcc'] = bcc_you
+    msg['Cc'] = CC
+    msg['Subject'] = subject
     text_part = MIMEText("Please view in an HTML capable mail reader", 'plain')
     html_part = MIMEText(html, 'html')
 
@@ -167,8 +168,12 @@ if __name__ == '__main__':
     sun2 = calcSunday()
     weeks = calcWeek(sun2)
     col = calcCol(weeks)
+    subject = "Plan for Sunday Service on " + sun2.strftime("%d %b")
+    print('Subject: {}'.format(subject))
     html = composeMessage(SPREADSHEET_ID, SHEET_NAME, weeks, col)
     try:
-      sendMail(html)
+      sendMail(subject, html)
     except:
-      print('Unable to send message')
+      if args.verbose:
+        print('Unable to send message, is there a local mail transport agent?')
+      sys.exit(-1)
